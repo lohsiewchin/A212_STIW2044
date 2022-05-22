444b7077cf66f5d346147ac36e3748dd94be86e2
lohsiewchin <73092542+lohsiewchin@users.noreply.github.com>
2022-05-22 09:26:40 +0800

    lab_assg_2
    MYTutor

---
+++ lab_assg_2/lib/views/loginScreen.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:lab_assg_2/views/registration.dart';
+
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../constants.dart';
+import '../models/reg.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({Key? key}) : super(key: key);
+  
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  late double screenHeight, screenWidth, ctrwidth;
+  bool remember = false;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    loadPref();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth >= 800) {
+      ctrwidth = screenWidth / 1.5;
+    }
+    if (screenWidth < 800) {
+      ctrwidth = screenWidth;
+    }
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Center(
+        child: SizedBox(
+          width: ctrwidth,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                          height: screenHeight / 3,
+                          width: screenWidth,
+                          child: Image.asset('assets/images/MY Tutor Logo.jpg')),
+
+                      const Text(
+                        "Login",
+                        style: TextStyle(fontSize: 24),
+                      ),
+
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter valid email';
+                          }
+                          bool emailValid = RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value);
+
+                          if (!emailValid) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 10,),
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          if (value.length < 6) {
+                            return "Password must be at least 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: remember,
+                            onChanged: (bool? value) {
+                              _onRememberMeChanged(value!);
+                            },
+                          ),
+                          const Text("Remember Me")
+                        ],
+                      ),
+                      SizedBox(
+                        width: screenWidth,
+                        height: 50,
+                        child: ElevatedButton(
+                          child: const Text("Login", style: TextStyle(fontSize: 24, color:Colors.black)),
+                          onPressed: _loginUser,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                 GestureDetector(
+                        child: const Text("Don't have an account? Register!",
+                            style: TextStyle(color: Colors.black, fontSize:20)),
+                        onTap: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const registration()));},
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Future<void> _saveRemovePref(bool value) async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      String email = emailController.text;
+      String password = passwordController.text;
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (value) {
+        await prefs.setString('email', email);
+        await prefs.setString('password', password);
+        await prefs.setBool('remember', true);
+        Fluttertoast.showToast(
+            msg: "Preference Stored",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            fontSize: 14.0);
+      } else {
+        await prefs.setString('email', '');
+        await prefs.setString('password', '');
+        await prefs.setBool('remember', false);
+        emailController.text = "";
+        passwordController.text = "";
+        Fluttertoast.showToast(
+            msg: "Preference Removed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            fontSize: 14.0);
+      }
+    } else {
+      Fluttertoast.showToast(
+          msg: "Preference Failed",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+      remember = false;
+    }
+  }
+
+  void _onRememberMeChanged(bool value) {
+    remember = value;
+    setState(() {
+      if (remember) {
+        _saveRemovePref(true);
+      } else {
+        _saveRemovePref(false);
+      }
+    });
+  }
+
+  Future<void> loadPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String email = (prefs.getString('email')) ?? '';
+    String password = (prefs.getString('password')) ?? '';
+    remember = (prefs.getBool('remember')) ?? false;
+
+    if (remember) {
+      setState(() {
+        emailController.text = email;
+        passwordController.text = password;
+        remember = true;
+      });
+    }
+  }
+
+  void _loginUser() {
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      http.post(
+        Uri.parse(CONSTANTS.server + "/MYTutor/users/php/login_user.php"),
+        body: {"email" : email, "password" : password}).then((response){
+          var data = jsonDecode(response.body);
+          if (response.statusCode == 200 && data['status'] == 'success') {
+            Registration reg = Registration.fromJson(data['data']);
+
+            Fluttertoast.showToast(
+              msg: "Login Success",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0
+            );
+          }
+          else{
+             Fluttertoast.showToast(
+              msg: "Login Failed",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0
+            );
+          }
+        });
+    }
+   }
+  }
+
+  
+
